100aad9a48882a4a2006b1e23b0bc5b7ea99bad7
Alejandro España <alejoespana123@gmail.com>
2025-05-19 02:49:05 +0000

    fix it

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,7 @@
 import type {NextConfig} from 'next';
 
 const nextConfig: NextConfig = {
+  output: 'export', // Added for static export
   /* config options here */
   typescript: {
     ignoreBuildErrors: true,
